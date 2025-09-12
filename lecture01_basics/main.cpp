7be63266fc7f350a1a7fb612ec2aa08de97f48eb
Миша Тарачков <mishklgpmi@mail.ru>
2025-09-12 19:33:15 +0200

    ADD: Hello, world, cmake

---
+++ lecture01_basics/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Показать про русский язык
+/*
+111
+222
+333
+*/
+
+int main() {
+
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "111" << std::endl;
+
+    return 0;
+}
